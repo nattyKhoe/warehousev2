38214c399bc236626451a5ed0aff26641648f84b
Natasha <natasha.khoe@gmail.com>
2023-02-16 15:58:41 +1100

    done with sql side.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,96 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_02_16_045809) do
+
+  create_table "invoice_in_line_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.decimal "price", precision: 10, scale: 2, default: "0.0"
+    t.decimal "total_amount", precision: 10, scale: 2, default: "0.0"
+    t.integer "invoice_in_id"
+    t.integer "item_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "invoice_ins", force: :cascade do |t|
+    t.string "invoice_number"
+    t.date "date"
+    t.date "due_date"
+    t.decimal "total", precision: 10, scale: 3, default: "0.0"
+    t.decimal "discount", precision: 2, scale: 3, default: "0.0"
+    t.decimal "tax", precision: 10, scale: 3, default: "0.0"
+    t.decimal "grand_total", precision: 10, scale: 3, default: "0.0"
+    t.integer "manufacturer_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "invoice_out_line_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.decimal "price", precision: 10, scale: 3, default: "0.0"
+    t.decimal "total_amount", precision: 10, scale: 3, default: "0.0"
+    t.integer "invoice_out_id"
+    t.integer "item_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "invoice_outs", force: :cascade do |t|
+    t.string "invoice_number"
+    t.date "date"
+    t.decimal "total", precision: 10, scale: 3, default: "0.0"
+    t.decimal "discount", precision: 2, scale: 3, default: "0.0"
+    t.decimal "tax", precision: 10, scale: 3, default: "0.0"
+    t.decimal "grand_total", precision: 10, scale: 3, default: "0.0"
+    t.boolean "paid_status"
+    t.integer "store_id"
+    t.string "payment_method"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "item_code"
+    t.string "item_name"
+    t.decimal "price", precision: 10, scale: 3, default: "0.0"
+    t.decimal "buying_price", precision: 10, scale: 3, default: "0.0"
+    t.integer "stock"
+    t.string "category"
+    t.integer "manufacturer_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "manufacturers", force: :cascade do |t|
+    t.string "tax_number"
+    t.string "manufacturer_code"
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string "tax_number"
+    t.string "store_code"
+    t.string "name"
+    t.decimal "discount", precision: 2, scale: 3, default: "0.0"
+    t.decimal "plafond", precision: 15, scale: 3, default: "0.0"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "account_type"
+    t.date "date_of_birth"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
